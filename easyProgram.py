0eee0e6d239ad53acb33cd8d2bb25091a9504c92
Phil <lfdyf20@163.com>
2021-06-23 20:21:45 +0800

    Fix: fix issues in easyProgram to adopt new format

--- easyProgram.py
+++ easyProgram.py
@@ -1,23 +1,35 @@
 import re
 import os
 
-# fileName = "Number of Islands.py"
-fileName = max([f for f in os.listdir('.') if re.match(r'[\w ]*\.py', f)], key = os.path.getctime)
+
+# Settings:
+USE_CREATION_TIME = False
+
+
+fileName = max([f for f in os.listdir('.') if re.match(r'[\w ]*\.py', f)], 
+	key = os.path.getctime if USE_CREATION_TIME else os.path.getmtime)
 print( fileName )
 with open( fileName, 'r' ) as f:
 	content = f.read()
 
-	match = re.search("def (\w+)\(self, ([, \w]+)\)", content)
+	match = re.search("def ([\w\d]+)\(self, ([\w\d: \[\],]+)\)", content)
 	matchSL = re.search("sl.=.Solution", content)
 	# print(matchSL.groups())
 	if matchSL == None and match:
 		funcName = match.group(1)
-		varName = match.group(2)
-		content
+		parameterInfos = match.group(2)
 		contentToAdd = "\n\n\n\n"
-		contentToAdd += varName + " = \n\n"
+
+		# add parameterInfo
+		parameterNames = []
+		for var in parameterInfos.split(", "):
+			parameterName = var.split(":")[0]
+			contentToAdd += parameterName + " = \n"
+			parameterNames.append(parameterName)
+
+		contentToAdd += "\n\n"
 		contentToAdd += "sl = Solution()\n"
-		contentToAdd += "print( sl." + funcName + "( " + varName+ " ) )" 
+		contentToAdd += "print( sl." + funcName + "( " + ", ".join(parameterNames)+ " ) )" 
 		with open( fileName, 'a' ) as f:
 			f.write(contentToAdd)
 
